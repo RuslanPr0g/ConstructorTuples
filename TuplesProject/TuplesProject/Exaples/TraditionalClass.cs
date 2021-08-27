7172357f756ad72e90633f8cd3f4033e35639cdb
Ruslan Rudenko <ruds@opinov8.com>
2021-08-27 18:37:20 +0300

    add examples

---
+++ TuplesProject/TuplesProject/Exaples/TraditionalClass.cs
@@ -0,0 +1,16 @@
+﻿namespace TuplesProject.Exaples
+{
+    class TraditionalClass
+    {
+        public TraditionalClass(string firstName, string middleName, string lastName)
+        {
+            FirstName = firstName;
+            MiddleName = middleName;
+            LastName = lastName;
+        }
+
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ TuplesProject/TuplesProject/Exaples/TuplesClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuplesProject.Exaples
+{
+    class TuplesClass
+    {
+        public TuplesClass(string firstName, string middleName, string lastName)
+        {
+            (FirstName, MiddleName, LastName) = (firstName, middleName, lastName);
+        }
+
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+    }
+}
