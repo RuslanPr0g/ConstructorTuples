f987c85d4d27ad135702e731fbb0ba86930464b7
Ruslan Rudenko <ruds@opinov8.com>
2021-08-27 18:38:55 +0300

    add example, fix usings

---
+++ TuplesProject/TuplesProject/Exaples/DeconstructClass.cs
@@ -0,0 +1,21 @@
+﻿namespace TuplesProject.Exaples
+{
+    class DeconstructClass
+    {
+        public DeconstructClass(string firstName, string middleName, string lastName)
+      => (FirstName, MiddleName, LastName) = (firstName, middleName, lastName);
+
+        public void Deconstruct(out string firstName,
+          out string middleName,
+          out string lastName)
+        {
+            firstName = FirstName;
+            middleName = MiddleName;
+            lastName = LastName;
+        }
+
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+    }
+}
--- TuplesProject/TuplesProject/Exaples/TuplesClass.cs
+++ TuplesProject/TuplesProject/Exaples/TuplesClass.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TuplesProject.Exaples
+﻿namespace TuplesProject.Exaples
 {
     class TuplesClass
     {
