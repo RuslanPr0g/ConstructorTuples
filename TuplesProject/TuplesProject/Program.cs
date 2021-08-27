e7f813aaf365216f724e2a7d5c8050f25b223643
Ruslan Rudenko <ruds@opinov8.com>
2021-08-27 18:32:45 +0300

    console app

---
+++ TuplesProject/TuplesProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TuplesProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Tuples!");
+        }
+    }
+}
